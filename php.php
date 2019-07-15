377acaf7ea154d261d6c90ba5cacc30fc851fa0a
aljhnme <52810715+aljhnme@users.noreply.github.com>
2019-07-15 02:33:38 -0700

    Add files via upload

---
+++ php.php
@@ -0,0 +1,516 @@
+
+
+1	count	
+
+The count() function returns the number of elements in an array.
+
+
+2	is_array
+
+
+The is_array() function is used to find whether a variable is an array or not.
+
+
+3	substr
+
+
+The substr() function returns a part of a string.
+
+
+
+4	in_array	
+
+
+The in_array() function searches an array for a specific value.
+
+
+
+5	explode	
+
+
+The in_array() function searches an array for a specific value.
+
+
+
+6	str_replace
+
+The str_replace() function replaces some characters with some other characters in a string.
+
+
+7	implode	
+
+
+The implode() function returns a string from the elements of an array.
+
+
+
+9	array_merge	
+
+
+The array_merge() function merges one or more arrays into one array.
+
+
+10	strpos	
+
+
+The strpos() function finds the position of the first occurrence of a string inside another string.
+
+
+11	preg_match
+
+
+The preg_match() function searches string for pattern, returning true if pattern exists, and false otherwise.
+
+
+12	sprintf	
+
+The sprintf() function writes a formatted string to a variable.
+
+
+13	trim
+
+
+The trim() function removes whitespace and other predefined characters from both sides of a string.
+
+
+14	strtolower
+
+
+The strtolower() function converts a string to lowercase.
+
+
+15	file_exists
+
+
+The file_exists() function checks whether or not a file or directory exists.
+
+
+16	is_string	
+
+
+The is_string () function is used to find whether a variable is a string or not.
+
+
+17	preg_replace
+
+The preg_replace() function operates just like POSIX function ereg_replace(), except that regular expressions can be used in the pattern and replacement input parameters.
+
+
+18	file_get_contents
+
+
+The file_get_contents() reads a file into a string.
+
+
+19	array_key_exists
+
+
+The array_key_exists() function is used to check whether a specified key is present in an array or not.
+
+
+
+20	array_keys	
+
+
+The array_keys() function returns an array containing the keys.
+
+
+21	dirname	
+
+
+The dirname() function returns the directory name from a path.
+
+
+22	function_exists	
+
+
+The function_exists() is an inbuilt function in PHP. The function_exists() function is useful in case if we want to check whether a function() exists or not in the PHP script. It is used to check for both built-in functions as well as user-defined functions.
+
+
+
+23	array_map
+
+
+The array_map() function sends each value of an array to a user-made function, and returns an array with new values, given by the user-made function.
+
+24	get_class
+
+The get_class() function gets the name of the class of an object. It returns FALSE if object is not an object. If object is excluded when inside a class, the name of that class is returned.
+
+25	class_exists
+
+
+This function checks if the given class have been defined. Returns TRUE if class_name is a defined class, FALSE otherwise.
+
+
+26	is_object
+
+
+The is_object() function is used to find whether a variable is an object or not.
+
+
+27	time
+
+
+The time() function returns the current time in the number of seconds since the Unix Epoch (January 1 1970 00:00:00 GMT).
+
+
+28	json_encode	
+
+
+json_encode — Returns the JSON representation of a value
+
+
+29	date
+
+
+date - Format a local time/date
+
+
+30	is_null	
+
+
+ The is_null () function is used to test whether a variable is NULL or not.
+
+
+31	is_numeric
+
+
+The is_numeric() function is used to check whether a variable is numeric or not.
+
+
+32	array_shift	
+
+The array_shift() function removes the first element from an array, and returns the value of the removed element.
+
+33	defined	
+
+The defined() function checks whether a constant exists.
+
+
+34	is_dir
+
+The is_dir() function checks whether the specified file is a directory.
+
+
+35	json_decode
+
+json_decode — Decodes a JSON string
+
+36	header
+
+The header() function sends a raw HTTP header to a client.
+
+
+37	strtoupper
+
+The strtoupper() function converts a string to uppercase.
+
+
+38	array_values
+
+
+This inbuilt function in PHP is used to get an array of values from another array that may contain key-value pairs or just values. The function creates another array where it stores all the values and by default assigns numerical keys to the values.
+
+
+39	md5	
+
+
+The md5() function calculates the MD5 hash of a string.
+
+
+40	method_exists
+
+
+method_exists — Checks if the class method exists
+
+
+41	file_put_contents
+
+
+The file_put_contents() writes a string to a file.
+
+
+42	rtrim
+
+The rtrim() function removes whitespace or other predefined characters from the right side of a string.
+
+
+43	array_pop
+
+The array_pop() function deletes the last element of an array.
+
+44	unlink	
+
+The unlink() function deletes a file.
+
+45	basename
+
+
+The basename() function returns the filename from a path.
+
+
+46	realpath
+
+The realpath() function returns the absolute pathname.
+
+
+47	call_user_func
+
+call_user_func — Call the callback given by the first parameter
+
+
+48	call_user_func_array
+
+
+call_user_func_array — Call a callback with an array of parameters
+
+
+49	fopen
+
+fopen — Opens file or URL
+
+50	microtime
+
+microtime — Return current Unix timestamp with microseconds
+
+51	fclose
+
+fclose — Closes an open file pointer
+
+
+52	is_int
+
+is_int — Find whether the type of a variable is integer
+
+
+53	is_file	
+
+is_file — Tells whether the filename is a regular file
+
+
+54	array_slice	
+
+array_slice — Extract a slice of the array
+
+
+55	preg_match_all
+
+preg_match_all — Perform a global regular expression match
+
+
+56	ucfirst	
+
+
+ucfirst — Make a string's first character uppercase
+
+
+57	intval
+
+intval — Get the integer value of a variable
+
+
+58	str_repeat
+
+str_repeat — Repeat a string
+
+
+59	serialize
+
+serialize — Generates a storable representation of a value
+
+
+60	array_filter
+
+array_filter — Filters elements of an array using a callback function
+
+61	mkdir
+
+mkdir — Makes directory
+
+
+62	is_callable	
+
+is_callable — Verify that the contents of a variable can be called as a function
+
+
+63	ltrim
+
+ltrim — Strip whitespace (or other characters) from the beginning of a string
+
+
+64	ob_start
+
+ob_start — Turn on output buffering
+
+
+65	round
+
+round — Rounds a float
+
+
+66	fwrite
+
+fwrite — Binary-safe file write
+
+
+67	array_unique
+
+array_unique — Removes duplicate values from an array
+
+
+68	array_search
+
+array_search — Searches the array for a given value and returns the first corresponding key if successful
+
+69	reset
+
+The reset() function used to move the array's internal pointer to the first element.
+
+
+70	array_unshif
+
+array_unshift — Prepend one or more elements to the beginning of an array
+
+
+71	parse_url
+
+
+parse_url — Parse a URL and return its components
+
+
+72	func_get_args
+
+func_get_args — Returns an array comprising a function's argument list
+
+
+73	end
+
+end — Set the internal pointer of an array to its last element
+
+
+74	base64_encode	
+
+base64_encode — Encodes data with MIME base64
+
+
+75	unserialize	
+
+unserialize — Creates a PHP value from a stored representation
+
+
+76	max
+
+max — Find highest value
+
+
+77	preg_split
+
+
+preg_split — Split string by a regular expression
+
+
+78	gettype
+
+gettype — Get the type of a variable
+
+
+79	strrpos
+
+strrpos — Find the position of the last occurrence of a substring in a string
+
+80	version_compare
+
+version_compare — Compares two "PHP-standardized" version number strings
+
+
+81	array_push
+
+array_push — Push one or more elements onto the end of array
+
+82	floor
+
+floor — Round fractions down
+
+
+83	strtotime
+
+strtotime — Parse about any English textual datetime description into a Unix timestamp
+
+
+84	htmlspecialchars
+
+htmlspecialchars — Convert special characters to HTML entities
+
+85	ini_get	
+
+ini_get — Gets the value of a configuration option
+
+86	ini_set	
+
+ini_set — Sets the value of a configuration option
+
+
+87	chr	
+
+chr — Generate a single-byte string from a number
+
+
+88	extension_loaded
+
+extension_loaded — Find out whether an extension is loaded
+
+
+89	is_bool	
+
+is_bool — Finds out whether a variable is a boolean
+
+
+90	ksort
+
+The ksort() function sorts an associative array in ascending order, according to the key.
+
+
+91	array_reverse
+
+The array_reverse() function is used to reverse the order of the elements in an array.
+
+
+92	ord	
+
+ord — Convert the first byte of a string to a value between 0 and 255
+
+
+93	uniqid
+
+uniqid — Generate a unique ID
+
+94	strtr
+
+The strtr() function translates certain characters in a string.
+
+95	array_diff
+
+array_diff — Computes the difference of arrays
+
+
+96	error_reporting	
+
+error_reporting — Sets which PHP errors are reported
+
+97	ceil
+
+ceil — Round fractions up
+
+98	urlencode
+
+urlencode — URL-encodes string
+
+99	min	
+
+min — Find lowest value
+
+100	print_r	
+
+print_r — Prints human-readable information about a variable
+
